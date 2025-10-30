8655dcbd85cac65b64d3608ac4a0916b8748ac74
Song1611 <doansong.1611@gmail.com>
2025-10-30 16:18:57 +0700

    feat(album): complete CRUD feature (controller, service, repository)

--- src/main/java/com/xuansong/musifyapi/controller/RootController.java
+++ src/main/java/com/xuansong/musifyapi/controller/RootController.java
@@ -1,4 +1,15 @@
 package com.xuansong.musifyapi.controller;
 
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
 public class RootController {
+    @GetMapping
+    public String healthyCheck() {
+        return "OK";
+    }
 }
